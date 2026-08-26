413f7677bb31b398d0b4580efa39c3594c506e10
Urmit Patel <urmitz1990@gmail.com>
2026-08-25 23:37:28 -0400

    Added JWT Authentication POST, PUT, DELETE http calls

--- routes/events.go
+++ routes/events.go
@@ -5,7 +5,6 @@ import (
 	"strconv"
 
 	"example.com/rest-api/models"
-	"example.com/rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -34,25 +33,14 @@ func getEvent(context *gin.Context) {
 
 func createEvent(context *gin.Context) {
 
-	token := context.Request.Header.Get("Authorization")
-
-	if token == "" {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized"})
-		return
-	}
-
-	UserID, err := utils.VerifyToken(token)
-	if err != nil {
-		context.JSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized"})
-		return
-	}
-
 	var event models.Event
-	err = context.ShouldBindJSON(&event)
+	err := context.ShouldBindJSON(&event)
 	if err != nil {
 		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse data"})
 		return
 	}
+
+	UserID := context.GetInt64("userID")
 
 	event.UserID = UserID
 	err = event.Save()
--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"net/http"
 
+	"example.com/rest-api/middlewares"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,9 +19,20 @@ func RegisterRoutes(server *gin.Engine) {
 
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent) // events/1   events/105
-	server.POST("/events", createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+
+	// Option 1 : Add Authenticate as 1st function to execute
+	//
+	// server.POST("/events", middlewares.Authenticate, createEvent)
+	// server.PUT("/events/:id", middlewares.Authenticate, updateEvent)
+	// server.DELETE("/events/:id", middlewares.Authenticate, deleteEvent)
+
+	//Option 2 : Group
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authenticate) // Executed before any requests in group is fulfilled
+	authenticated.POST("/events", createEvent)
+	authenticated.PUT("/events/:id", updateEvent)
+	authenticated.DELETE("/events/:id", deleteEvent)
+
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
